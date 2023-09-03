1ba4a582107578d669cc5a2784070e0298f170e2
Peris Wakio <peris.kingori@student.moringaschool.com>
2023-09-03 19:12:48 +0000

    complete d assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,24 @@
+import react from "react";
+import Article from "./Article";
+
+
+
+
+function ArticleList({posts}){
+
+    return(
+        <main>
+
+            {
+                posts.map(article=>{
+                    return <Article key={article.id} title={article.title} date={article.date} preview={article.preview}/>
+                })
+            }
+
+
+        </main>
+
+    )
+}
+
+export default ArticleList
